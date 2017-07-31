19d953a8a7e69f25a4e7395d023a37cd20fb4c6b
Miguel Mota <miguelmota2@gmail.com>
2017-07-31 14:09:38 -0700

    style

--- src/components/PublisherApplicationInfo.js
+++ src/components/PublisherApplicationInfo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import './PublisherApplicationInfo.css'
+
 class PublisherApplicationInfo extends Component {
   render () {
     return (
@@ -8,6 +10,10 @@ class PublisherApplicationInfo extends Component {
           <div className='column sixteen wide left aligned'>
             <div className="ui large header">
               adChain Benefits the Publisher
+            </div>
+          </div>
+          <div className='column sixteen wide'>
+            <div className='ui grid'>
             </div>
           </div>
         </div>
