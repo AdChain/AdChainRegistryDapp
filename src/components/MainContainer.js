3c2ebf81b51b8f54d276b463ae8e8fbebba81f92
Miguel Mota <miguelmota2@gmail.com>
2017-08-01 15:50:41 -0700

    sidebar active class

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import React from 'react'
 
 import MainTopbar from './MainTopbar'
 import ApplicationContainer from './ApplicationContainer'
 import DomainsContainer from './DomainsContainer'
 
-class MainContainer extends Component {
-  render () {
-    return (
-      <Router>
-        <div className='ui grid'>
-          <MainTopbar />
-          <Route path='/' exact={true} component={DomainsContainer} />
-          <Route path='/apply' exact={true} component={ApplicationContainer} />
-          <Route path='/domains' exact={true} component={DomainsContainer} />
-        </div>
-      </Router>
-    )
-  }
+function MainContainer (props) {
+  const Route = props.Route
+
+  return (
+    <div className='ui grid'>
+      <MainTopbar />
+      <Route path='/' exact={true} component={DomainsContainer} />
+      <Route path='/apply' exact={true} component={ApplicationContainer} />
+      <Route path='/domains' exact={true} component={DomainsContainer} />
+    </div>
+  )
 }
 
 export default MainContainer
