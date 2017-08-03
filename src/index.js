2cef94209e2ee068b69f0b37c5f2b45514ee6095
Miguel Mota <miguelmota2@gmail.com>
2017-08-03 12:54:40 -0700

    ad block alert

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+
 import registerServiceWorker from './registerServiceWorker'
+import App from './App'
+import AdBlockAlert from './AdBlockAlert'
+
+import './index.css'
+
+function adBlockDetected () {
+  ReactDOM.render(<AdBlockAlert />, document.getElementById('root'))
+}
+
+if (typeof window.fuckAdBlock === 'undefined') {
+  adBlockDetected()
+} else {
+  window.fuckAdBlock.onDetected(adBlockDetected)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
