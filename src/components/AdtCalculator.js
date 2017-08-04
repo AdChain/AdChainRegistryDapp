5cce51828cd27706a23507c2a0a840357627c004
Miguel Mota <miguelmota2@gmail.com>
2017-08-03 23:07:04 -0700

    adt conversion

--- src/components/AdtCalculator.js
+++ src/components/AdtCalculator.js
@@ -1,60 +1,190 @@
 import React, { Component } from 'react'
 import commafy from 'commafy'
+import {Dropdown} from 'semantic-ui-react'
 
+import priceStats from '../services/priceStats'
 import toCurrency from '../utils/toCurrency'
 
 import './AdtCalculator.css'
 
-class AdtCalculator extends Component {
-  render () {
-    const usdEth = 34231
-    const ethAdt = 35443
-    const usdAdt = 45632
+function formatValue (value) {
+  return commafy(+(value).toFixed(5))
+}
 
-    const conversionEth = null
-    const conversionAdt = null
+class AdtCalculator extends Component {
+  constructor (props) {
+    super()
+
+    const ethUsd = 0
+    const adtUsd = 0
+    const adtEth = adtUsd / ethUsd
+    const ethAdt = 1 / adtEth
+    const usdEth = 1 / ethUsd
+    const usdAdt = 1 / adtUsd
+
+    const options = [{
+      text: 'USD',
+      value: 'usd'
+    }, {
+      text: 'ETH',
+      value: 'eth'
+    }, {
+      text: 'ADT',
+      value: 'adt'
+    }]
+
+    this.state = {
+      options,
+      selectedOption: 'usd',
+      inputValue: 0,
+      ethUsd,
+      ethAdt,
+      adtEth,
+      adtUsd,
+      usdEth,
+      usdAdt,
+      conversionUsd: 0,
+      conversionEth: 0,
+      conversionAdt: 0
+    }
+
+    this.calculate = this.calculate.bind(this)
+    this.getPrices = this.getPrices.bind(this)
+
+    this.getPrices()
+  }
+
+  getPrices () {
+    priceStats()
+    .then(({ethUsd, adtUsd}) => {
+      const adtEth = adtUsd / ethUsd
+      const ethAdt = 1 / adtEth
+      const usdEth = 1 / ethUsd
+      const usdAdt = 1 / adtUsd
+
+      this.setState({
+        ethUsd,
+        adtUsd,
+        adtEth,
+        ethAdt,
+        usdEth,
+        usdAdt,
+      })
+
+      setTimeout(() => this.getPrices(), 25e3)
+    })
+  }
+
+  render () {
+    const {
+      options,
+      selectedOption,
+      ethUsd,
+      adtEth,
+      adtUsd,
+      conversionUsd,
+      conversionEth,
+      conversionAdt
+    } = this.state
 
     return (
       <div className='AdtCalculator BoxFrame'>
         <div className='ui grid stackable'>
           <div className='row'>
-            <div className='column eight wide'>
-              USD/ETHER: {commafy(usdEth)}
+            <div className='column seven wide'>
+              ETH/USD: {ethUsd ? toCurrency(ethUsd) : '-'}
             </div>
-            <div className='column eight wide'>
+            <div className='column nine wide'>
               <div className='ui right labeled mini input'>
-                <input type='text' placeholder='100' />
-                <div className='ui dropdown label blue'>
-                  <div className='text'>USD</div>
-                  <i className='dropdown icon' />
-                  <div className='menu'>
-                    <div className='item'>USD</div>
-                    <div className='item'>ETH</div>
-                    <div className='item'>ADT</div>
-                  </div>
-                </div>
+                <input
+                  type='text'
+                  placeholder='100'
+                  defaultValue={''}
+                  onInput={this.onInput.bind(this)}
+                />
+                <Dropdown
+                  className='label blue'
+                  value={selectedOption}
+                  options={options}
+                  onChange={this.onDropdownChange.bind(this)}
+                />
               </div>
             </div>
           </div>
           <div className='row'>
-            <div className='column eight wide'>
-              ETHER/ADT: {commafy(ethAdt)}Ξ
+            <div className='column seven wide'>
+              ADT/ETH: {adtUsd ? formatValue(adtEth) : '-'}Ξ
             </div>
-            <div className='column eight wide'>
-              Amount in ETH: {commafy(conversionEth)}
+            <div className='column nine wide'>
+              {selectedOption === 'eth' || selectedOption === 'adt' ? <span>Amount in USD: {toCurrency(conversionUsd)}</span> : <span>Amount in ETH: {formatValue(conversionEth)}Ξ</span>}
             </div>
           </div>
           <div className='row'>
-            <div className='column eight wide'>
-              USD/ADT: {toCurrency(usdAdt)}
+            <div className='column seven wide'>
+              ADT/USD: {adtUsd ? toCurrency(adtUsd) : '-'}
             </div>
-            <div className='column eight wide'>
-              Amount in ADT: {commafy(conversionAdt)}
+            <div className='column nine wide'>
+              {selectedOption === 'usd' || selectedOption === 'eth' ? <span>Amount in ADT: {formatValue(conversionAdt)}</span> : <span>Amount in ETH: {formatValue(conversionEth)}</span>}
             </div>
           </div>
         </div>
       </div>
     )
+  }
+
+  onDropdownChange (event, {value}) {
+    this.setState({
+      selectedOption: value
+    })
+
+    setTimeout(() => this.calculate(), 0)
+  }
+
+  onInput (event) {
+    event.preventDefault()
+
+    const {target} = event
+    let {value} = target
+
+    if (!value) {
+      value = 0
+    }
+
+    this.setState({
+      inputValue: parseFloat(value, 10)
+    })
+
+    setTimeout(() => this.calculate(), 0)
+  }
+
+  calculate () {
+    const {
+      ethUsd,
+      ethAdt,
+      adtEth,
+      adtUsd,
+      usdEth,
+      usdAdt,
+      selectedOption,
+      inputValue
+    } = this.state
+
+    if (selectedOption === 'usd') {
+      this.setState({
+        conversionEth: usdEth * inputValue,
+        conversionAdt: usdAdt * inputValue
+      })
+    } else if (selectedOption === 'eth') {
+      this.setState({
+        conversionUsd: ethUsd * inputValue,
+        conversionAdt: ethAdt * inputValue
+      })
+    } else if (selectedOption === 'adt') {
+      this.setState({
+        conversionUsd: adtUsd * inputValue,
+        conversionEth: adtEth * inputValue
+      })
+    }
   }
 }
 
