df6d873e084d99af64cbc26686b9a49a18a7b7bc
Miguel Mota <miguelmota2@gmail.com>
2017-08-10 14:13:03 -0700

    stat progress bar

---
+++ src/components/StatProgressBar.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+import './StatProgressBar.css'
+
+class StatProgressBar extends Component {
+  constructor (props) {
+    super()
+
+    this.state = {
+      fills: props.fills,
+      fill: props.fill,
+      showFillLabels: props.showFillLabels,
+      showLegend: props.showLegend,
+      fillLabels: props.fillLabels
+    }
+  }
+
+  componentWillReceiveProps (props) {
+    this.setState({
+      fills: props.fills,
+      fill: props.fill,
+      showFillLabels: props.showFillLabels,
+      showLegend: props.showLegend,
+      fillLabels: props.fillLabels
+    })
+  }
+
+  render () {
+    const {
+      fills,
+      fill,
+      showFillLabels,
+      showLegend,
+      fillLabels
+    } = this.state
+
+    return (
+      <div className='StatProgressBar'>
+        <div className='StatBarContainer'>
+          {fills ?
+            [<div
+              style={{width: `${fills[0]}%`}}
+              title={`${fills[0]}%`}
+              className='StatBarFillContainer'>
+              {showFillLabels ? `${fills[0]}%` : null}
+            </div>,
+            <div
+              style={{width: `${fills[1]}%`}}
+              title={`${fills[1]}%`}
+              className='StatBarFillContainer'>
+              {showFillLabels ? `${fills[1]}%` : null}
+            </div>]
+          :
+            <div
+              style={{width: `${fill}%`}}
+              className='StatBarFillContainer Round'>
+              {showFillLabels ? `${fill}%` : null}
+            </div>
+          }
+        </div>
+        {showLegend ?
+          <div className='StatBarLegend'>
+            <label className='FillLabel'>
+              {fillLabels[0]}
+            </label>
+            <label className='FillLabel'>
+              {fillLabels[1]}
+            </label>
+          </div>
+          : null}
+      </div>
+    )
+  }
+}
+
+export default StatProgressBar
