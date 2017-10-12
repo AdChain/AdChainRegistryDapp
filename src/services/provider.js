409c3010b19502490287026cf6f7674b6f41afe1
Isaac Kang <isaac.kang@consensys.net>
2017-10-12 10:16:12 -0400

    Add ethjs. webpack syntax

--- src/services/provider.js
+++ src/services/provider.js
@@ -1,12 +1,11 @@
-const { getProviderUrl } = require('../config')
+import Eth from 'ethjs'
+import { getProviderUrl } from '../config'
 
-module.exports = {
-  getProvider () {
-    if (window.web3) {
-      return window.web3.currentProvider
-    } else {
-      const provider = new window.Web3.providers.HttpProvider(getProviderUrl())
-      return provider
-    }
+export const getProvider = () => {
+  if (window.web3) {
+    return window.web3.currentProvider
+  } else {
+    const provider = new Eth.HttpProvider(getProviderUrl())
+    return provider
   }
 }
