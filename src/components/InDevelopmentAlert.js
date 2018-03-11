2635cc871e709545fa38a3e92f35c9788f1dab34
Nicholas Porter <nicholas@metax.io>
2018-03-10 16:18:25 -0800

    Modified some messaging

--- src/components/InDevelopmentAlert.js
+++ src/components/InDevelopmentAlert.js
@@ -2,35 +2,34 @@ import React, { Component } from 'react'
 import { Button, Header, Modal, Icon } from 'semantic-ui-react'
 
 class InDevelopmentAlert extends Component {
-  constructor(){
+  constructor () {
     super()
     this.state = {
       open: true
     }
   }
 
-  close(){
-    this.setState({open:false})
+  close () {
+    this.setState({open: false})
   }
- render(){
-  return (
-    <Modal open={this.state.open}>
-      <Header icon='configure' content='Under Development' />
-      <Modal.Content >
-        <p style={{padding:'20px',fontSize: '16px'}}>
+  render () {
+    return (
+      <Modal open={this.state.open}>
+        <Header icon='configure' content='Under Development' />
+        <Modal.Content >
+          <p style={{padding: '20px', fontSize: '16px'}}>
         The adChain Registry is currently under development as we prepare to launch on the Main Ethereum Network.
-        <br/><br/> This DApp will remain intermittently functional until April/May.
+        <br /><br /> This DApp will be intermittently functional until April/May.
         </p>
-      </Modal.Content>
-      <Modal.Actions>
-      <Button color='green' inverted onClick={this.close.bind(this)}>
-        <Icon name='checkmark' /> OK
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='green' inverted onClick={this.close.bind(this)}>
+            <Icon name='checkmark' /> OK
       </Button>
-    </Modal.Actions>
-    </Modal>
-  )
- } 
-
+        </Modal.Actions>
+      </Modal>
+    )
+  }
 }
 
 export default InDevelopmentAlert
