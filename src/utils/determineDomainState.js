e171185ea60f65f61b9806463f58761e70db2a72
Nicholas Porter <nicholas@metax.io>
2018-03-22 15:48:26 -0700

    Added in the GA tag

--- src/utils/determineDomainState.js
+++ src/utils/determineDomainState.js
@@ -18,6 +18,7 @@ const getDomainState = async (domain) => {
     action: null,
     siteName: domain,
     stageEnds: null,
+    challenged: null,
     actionLabel: null,
     stageMapSrc: null,
     stageEndsTimestamp: null
@@ -31,6 +32,7 @@ const getDomainState = async (domain) => {
     const commitOpen = await registry.commitStageActive(domain)
     const revealOpen = await registry.revealStageActive(domain)
     const isInRegistry = (isWhitelisted)
+    item.challenged = challengeId > 0
 
 // -----------------------------------------
 // -----------------------------------------
@@ -72,6 +74,11 @@ const getDomainState = async (domain) => {
           item.label = <span><i className='icon check circle' style={{color: 'green'}} />In Registry</span>
         }
       } else {
+        if (item.challenged) {
+          item.stageMapSrc = 'MapInRegistryChallenge'
+        } else {
+          item.stageMapSrc = 'MapInRegistryNoChallenge'
+        }
         item.deposit = listing.currentDeposit
         item.stage = 'in_registry'
         item.label = <span><i className='icon check circle' style={{color: 'green'}} />In Registry</span>
@@ -83,11 +90,13 @@ const getDomainState = async (domain) => {
       if (isExpired(applicationExpiry)) {
         item.actionLabel = 'REFRESH STATUS'
         item.color = 'greyblack'
+        item.stageMapSrc = 'MapInApplicationPending'
       } else {
         item.label = 'In Application'
         item.color = 'red'
         item.stage = 'in_application'
         item.actionLabel = 'CHALLENGE'
+        item.stageMapSrc = 'MapInApplication'
       }
       item.stageEndsTimestamp = applicationExpiry
       item.stageEnds = moment.unix(applicationExpiry).format('YYYY-MM-DD HH:mm:ss')
@@ -102,19 +111,26 @@ const getDomainState = async (domain) => {
       item.label = 'Vote - Commit'
       item.color = 'blue'
       item.actionLabel = 'VOTE'
+      item.stageMapSrc = 'MapVoting'
 
 // -----------------------------------------
 // -----------------------------------------
     } else if (revealOpen) {
-      let { revealEndDate, votesFor, votesAgainst } = await registry.getChallengePoll(domain)
-      item.stage = 'voting_reveal'
-      item.stageEndsTimestamp = revealEndDate
-      item.stageEnds = moment.unix(revealEndDate).format('YYYY-MM-DD HH:mm:ss')
-      item.stats = { votesFor, votesAgainst }
-      item.label = 'Vote - Reveal'
-      item.color = 'green'
-      item.actionLabel = 'REVEAL'
-
+      if (isExpired(item.stageEndsTimestamp)) {
+        item.actionLabel = 'REFRESH STATUS'
+        item.color = 'greyblack'
+        item.stageMapSrc = 'MapRevealPending'
+      } else {
+        item.stageMapSrc = 'MapReveal'
+        let { revealEndDate, votesFor, votesAgainst } = await registry.getChallengePoll(domain)
+        item.stage = 'voting_reveal'
+        item.stageEndsTimestamp = revealEndDate
+        item.stageEnds = moment.unix(revealEndDate).format('YYYY-MM-DD HH:mm:ss')
+        item.stats = { votesFor, votesAgainst }
+        item.label = 'Vote - Reveal'
+        item.color = 'green'
+        item.actionLabel = 'REVEAL'
+      }
 // -----------------------------------------
 // -----------------------------------------
     } else if (applicationExists) {
