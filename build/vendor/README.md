61cbd21f1b11180ce0e37552a061aea0929b37d0
Nicholas Porter <nicholas@metax.io>
2018-04-20 10:23:47 -0700

    Publisher repo init, and deployment pipeline. Only splash page for website

--- build/vendor/README.md
+++
@@ -1,1 +0,0 @@
-`themes/` contains semantic ui fonts
--- src/models/urls.js
+++
@@ -1,6 +0,0 @@
-
-export const utiltyApiURL = 'https://utility-staging.adchain.com/'
-
-export const registryApiURL = 'https://adchain-registry-api-staging.metax.io/'
-
-export const contractsURL = 'https://s3-us-west-2.amazonaws.com/tcr-contracts-rinkeby/'
