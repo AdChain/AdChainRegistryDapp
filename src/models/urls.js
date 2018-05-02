1c3edc35be33b8e9704bdbd46c56f05c954ab5d1
Nicholas Porter <nicholas@metax.io>
2018-05-02 08:24:43 -0700

    Staging utility api switched from prod to staging

--- src/models/urls.js
+++ src/models/urls.js
@@ -1,5 +1,4 @@
-// export const utilityApiURL = process.env.REACT_APP_UTILITY_API_URL
-export const utilityApiURL = 'https://utility.adchain.com'
+export const utilityApiURL = process.env.REACT_APP_UTILITY_API_URL
 
 export const registryApiURL = process.env.REACT_APP_REGISTRY_API_URL
 
