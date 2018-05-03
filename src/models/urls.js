a92b53d1632543e9045d044b5fba3bec30e37fcf
Nicholas Porter <nicholas@metax.io>
2018-05-03 13:06:50 -0700

    Forgot to change the dev url back to prod

--- src/models/urls.js
+++ src/models/urls.js
@@ -1,5 +1,5 @@
-// export const utilityApiURL = process.env.REACT_APP_UTILITY_API_URL
-export const utilityApiURL = 'http://localhost:8000'
+export const utilityApiURL = process.env.REACT_APP_UTILITY_API_URL
+// export const utilityApiURL = 'http://localhost:8000'
 
 export const registryApiURL = process.env.REACT_APP_REGISTRY_API_URL
 
