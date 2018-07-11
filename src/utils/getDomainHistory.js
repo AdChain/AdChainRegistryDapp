56eacc68a81962589433d2b7a6b434beb2741d98
Nicholas Porter <nicholas@metax.io>
2018-07-11 12:33:48 -0700

    Added in error catchin for history api

--- src/utils/getDomainHistory.js
+++ src/utils/getDomainHistory.js
@@ -5,6 +5,13 @@ import {registryApiURL} from '../models/urls'
  * Return domain's history data
 */
 export const getDomainHistory = async (listingHash) => {
-  let res = await (await window.fetch(`${registryApiURL}/registry/domain/history?listing_hash=${listingHash}`)).json()
-  return res.history
+  let res
+  try {
+    res = await (await window.fetch(`${registryApiURL}/registry/domain/history?listing_hash=${listingHash}`)).json()
+    return res.history
+  } catch (error) {
+    res = []
+    console.log(error)
+  }
+  return res
 }
